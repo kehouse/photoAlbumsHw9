012edc56ed0a7d0b4ee2526067f3c08e64d0df66
Kathleen House <kathleenhouse@Macintosh-141.local>
2016-02-21 23:16:19 -0500

    click album will open album to show pictures

--- main.js
+++ main.js
@@ -4,21 +4,51 @@ $(document).ready(function() {
 
 var imageAlbums = "";
 
-albums.forEach(function (element, idx){
-  imageAlbums += "<ul>" + "<li><a href><img src= '" + element.albumCover + "' /></a>" + "<h3>" + element.albumTitle + "</h3>" + "</li>" + "</ul>";
-})
+_.each(albums, function (element){
+  imageAlbums += "<ul>"
+  + "<div class='albumClass' rel='"
+  + element.albumRel + "'>"
+  + "<li><img src= '"
+  + element.albumCover
+  + "' />"
+  + "<h3>"
+  + element.albumTitle
+  + "</h3>"
+  + "</li>"
+  + "</div>"
+  + "</ul>";
+});
 
-$('.container').append(imageAlbums);
+$('.home').append(imageAlbums);
 
 // To click the albums to show each album
 
-var navItem = $('li').find('img');
-navItem.click(function(event){
+var selectedPage = "";
+$('.albumClass').on('click', albums, function(event){
   event.preventDefault();
-  var selectedPage = "." + $(this).attr('a');
-
-  $(selectedPage).siblings('section').removeClass('active');
-  $(selectedPage).addClass('active');
+  $("section").removeClass('active');
+  $('.viewAlbum').addClass('active');
+  selectedPage = $(this).attr('rel');
+  setPhotoDisplay(selectedPage);
 });
+
+var getAlbumPhotos = function (albumclicked) {
+  var photoArray = albums.filter(function(element){
+    return element.albumRel === albumclicked;
+  });
+  return photoArray[0].photos;
+};
+
+var setPhotoDisplay = function (albumselected){
+  var photoDisplay = "";
+  _.each(getAlbumPhotos(selectedPage), function(element){
+    photoDisplay += "<div class='photoClass' id='" + element.photoRel + "'>"
+    + "<img src ='" + element.photoThumb + "'>"
+    + "<h3>" + element.photoName + "</h3>"
+    + "</div>"
+  });
+  $(".imagesClass").html(photoDisplay);
+};
+
 
 });
